fb2bd9715222dc3fedbc63be1e2b422262f22b38
to nguyen duc manh <ducmanh14032000@gmail.com>
2024-07-16 19:52:51 +0700

    create singleton pattern

---
+++ SingletonDP/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDP
+{
+    /// <summary>
+    /// class theo mô hình SingletonPattern
+    /// </summary>
+    public class Singleton
+    {
+        /// <summary>
+        /// đối tượng khởi tạo 1 lần duy nhất
+        /// </summary>
+        private static Singleton? instance;
+
+        /// <summary>
+        /// đối tượng trỏ tới đối tượng khởi tạo 1 lần duy nhất
+        /// </summary>
+        public static Singleton Instance
+        {
+            get
+            {
+                // kiểm tra xem đã khởi tạo lần nào chưa
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+    }
+}
