afe589e12f8378770db5b6211f84ab7114370c11
to nguyen duc manh <ducmanh14032000@gmail.com>
2024-07-16 19:58:43 +0700

    tạo unit test

---
+++ DesignPatternTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using DesignPattern;
+
+namespace DesignPatternTest
+{
+    /// <summary>
+    /// class test các design pattern
+    /// </summary>
+    [TestClass]
+    public class DesignPatternTestSimple
+    {
+        /// <summary>
+        /// kiểm tra xem có phải chỉ có 1 instance duy nhất không
+        /// </summary>
+        [TestMethod]
+        public void TestSingleton_OneInstance()
+        {
+            Singleton singleton = Singleton.Instance;
+            Singleton singleton2 = Singleton.Instance;
+
+            Assert.AreEqual(singleton, singleton2);
+        }
+    }
+}
--- SingletonDP/Singleton.cs
+++ SingletonDP/Singleton.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SingletonDP
+namespace DesignPattern
 {
     /// <summary>
     /// class theo mô hình SingletonPattern
